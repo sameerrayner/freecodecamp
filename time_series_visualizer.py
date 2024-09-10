ec19da0c008f7b69d8b563016fe00f6825540a2b
sameerrayner <166298491+sameerrayner@users.noreply.github.com>
2024-09-10 11:08:12 +0100

    Add files via upload

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,7 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = read_csv('fcc-forum-pageviews.csv', index_col = 'date', parse_dates = True)
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col = 'date', parse_dates = True)
 
 # Clean data
 df = df[(df.value >= df.value.quantile(0.025)) & (df.value <= df.value.quantile(0.975))]
